eba427683e6cedfa6586c47ab2dda21774e2a2a0
ZZZ <92006244+Chow-JIN@users.noreply.github.com>
2024-02-21 23:51:12 -0800

    Update shape.cpp

--- src/shape.cpp
+++ src/shape.cpp
@@ -11,5 +11,5 @@ Shape::Shape(Point center, std::string name): center(center), name(name)
 
 
 void Shape::print(std::ostream& out) const{
-	out << name << "(" << center.x << ", " << center.y << ")" << "area = "<< this->area() << endl;
+	out << name << " at (" << center.x << ", " << center.y << ")" << "area = "<< this->area() << endl;
 }

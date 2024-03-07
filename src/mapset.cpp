035707bf39d92193b041347dea7d215230016bf1
ZZZ <92006244+Chow-JIN@users.noreply.github.com>
2024-03-06 23:58:26 -0800

    Update mapset.cpp

--- src/mapset.cpp
+++ src/mapset.cpp
@@ -11,13 +11,11 @@ string to_lowercase(const string& str){
 	}
 
 set<string> load_stopwords(istream& stopwords){
-	set<string> out;
-    string word;
-    while (stopwords >> word) {
-        out.insert(to_lowercase(word));
-    }
-    return out;
-	}
+	{
+    set<string> s;
+    transform(istream_iterator<string>(stopwords), istream_iterator<string>(), inserter(s, s.end()), to_lowercase);
+    return s;
+}
 
 
 map<string, int> count_words(istream& document, const set<string>& stopwords){
